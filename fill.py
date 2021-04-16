9b2a99f60cb5e18391b5e5fff192e53e0c65404a
dorthy99 <mhahn@tfssa.org>
2021-04-16 11:46:22 -0500

    test

--- fill.py
+++ fill.py
@@ -7,37 +7,36 @@ pixels = neopixel.NeoPixel(board.D18, 20)
 for i in range (500):
         for i in range(19,-1,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,0,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,1,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,2,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,3,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,4,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,5,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(1)
+                time.sleep(.05)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
-        
