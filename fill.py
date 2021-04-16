1e5a00d091dfb5bec0c0d6659052e58138148df8
dorthy99 <mhahn@tfssa.org>
2021-04-16 11:49:02 -0500

    test

--- fill.py
+++ fill.py
@@ -7,27 +7,27 @@ pixels = neopixel.NeoPixel(board.D18, 20)
 for i in range (500):
         for i in range(19,-1,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,0,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,1,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,2,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,3,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
         for i in range(19,4,-1):
@@ -37,6 +37,71 @@ for i in range (500):
         pixels[i] = (0,0,255)
         for i in range(19,5,-1):
                 pixels[i] = (0,0,255)
-                time.sleep(.05)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,6,255)
+        for i in range(19,5,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,7,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,8,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,9,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,10,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,11,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,13,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,14,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,15,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,16,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,17,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,18,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
+                pixels[i] = (0,0,0)
+        pixels[i] = (0,0,255)
+        for i in range(19,19,-1):
+                pixels[i] = (0,0,255)
+                time.sleep(.03)
                 pixels[i] = (0,0,0)
         pixels[i] = (0,0,255)
