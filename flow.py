1f632aeaf02d299253246c2fc3fb12627ca929a6
dorthy99 <mhahn@tfssa.org>
2021-05-19 13:13:47 -0500

    f

---
+++ flow.py
@@ -0,0 +1,26 @@
+import board
+import neopixel
+import time
+n = 20
+pixels = neopixel.NeoPixel(board.D18, n)
+
+
+def fillup(dt):
+    for i in range(n):
+        pixels[i] = (100,255,0)
+        time.sleep(dt)
+def clear():
+    for i in range(n):
+        pixels[i] = (0,0,0)
+def xtwo():
+    for x in range(n):
+        r = x**2
+        if r < 0:
+            r = 0
+        if r > 255:
+            r = 255
+        pixels[x] = (r,0,0)
+
+fillup(.1)
+print("h")
+fillup(.5)
